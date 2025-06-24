a032f1498ff5a921fb756cac0e3ad76fb5a42aea
korodiy <korodiy@yandex-team.ru>
2025-06-25 01:01:47 +0300

    KS-5-1-if-else-when: Решила первую задачу.

---
+++ src/main/kotlin/KS-5-1-if-else-when.kt
@@ -0,0 +1,17 @@
+package org.example
+
+import kotlin.random.Random
+
+fun main() {
+    val firstNumber = Random.nextInt(0, 20)
+    val secondNumber = Random.nextInt(0, 20)
+    val twoCalculated = firstNumber + secondNumber
+
+    println("Докажи, что ты не робот! Сколько будет $firstNumber + $secondNumber?")
+    println("Твой ответ: ")
+
+    val userAnswer = readln().toInt()
+
+    if (userAnswer == twoCalculated) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
+}
