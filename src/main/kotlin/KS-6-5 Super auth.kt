0e1d436b42b9a8e954ff7d1b0b81d13e590a599d
korodiy <korodiy@yandex-team.ru>
2025-07-02 18:09:54 +0300

    KS-6-5 Super auth: Решение задачи про проверку на робота

---
+++ src/main/kotlin/KS-6-5 Super auth.kt
@@ -0,0 +1,25 @@
+package org.example
+
+fun main() {
+
+    var counter = 0
+
+    while (counter < 3 )  {
+        val firstNumber = (1..9).random()
+        val secondNumber = (1..9).random()
+        val summNumbers = firstNumber + secondNumber
+
+        println("Докажи, что ты не робот! Сколько будет $firstNumber + $secondNumber?")
+        val userAnswer = readln().toInt()
+
+        if (summNumbers==userAnswer) {
+            println("Добро пожаловать!")
+            break
+        } else if (counter < 2) {
+            counter++
+        } else {
+            println("Доступ запрещен.")
+            break
+        }
+    }
+}
