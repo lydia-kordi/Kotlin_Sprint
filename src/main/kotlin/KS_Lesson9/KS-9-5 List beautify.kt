ba43282a5116db7d36379b5a2cf755a6f47ad9d7
korodiy <korodiy@yandex-team.ru>
2025-07-10 16:40:03 +0300

    KS-9-5 List beautify: Исправленное форматирование коллекции

--- src/main/kotlin/KS_Lesson9/KS-9-5 List beautify.kt
+++ src/main/kotlin/KS_Lesson9/KS-9-5 List beautify.kt
@@ -2,21 +2,15 @@ package org.example.KS_Lesson9
 
 fun main() {
 
-    val ingredients: MutableList<String> = mutableListOf()
+    val ingredients: MutableSet<String> = mutableSetOf()
     val ingredientsNumber = 5
 
     for (i in 1..ingredientsNumber) {
-        while (true) {
             println("Введите ингредиент #$i:")
             val userInput = readln().trim()
-            if (ingredients.contains(userInput)) {
-                println("Этот ингредиент уже был добавлен. Придумайте другой:")
-            } else {
-                ingredients += userInput
-                break
-            }
+        ingredients += userInput
         }
-    }
+
     val sortedIngredients = ingredients
         .sorted()
         .toMutableList()
