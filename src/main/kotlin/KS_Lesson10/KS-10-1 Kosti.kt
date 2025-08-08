8907a8ce5c667a44652e36cde4e9c1d19eb8b878
korodiy <korodiy@yandex-team.ru>
2025-08-08 17:32:26 +0300

    KS-10-1 Использование функций в первый раз

---
+++ src/main/kotlin/KS_Lesson10/KS-10-1 Kosti.kt
@@ -0,0 +1,20 @@
+package org.example.KS_Lesson10
+
+fun main() {
+    val userRoll = diceRoll()
+    println("Игрок выбросил число: $userRoll")
+
+    val computerRoll = diceRoll()
+    println("Компьютер выбросил число: $computerRoll")
+
+    resultCheck(userRoll, computerRoll)
+}
+fun diceRoll (): Int = (1..6).random()
+
+fun resultCheck(userRoll: Int, computerRoll: Int) {
+    when {
+        userRoll > computerRoll -> println("Победило человечество")
+        userRoll < computerRoll -> println("Победила машина")
+        else -> println("Победила дружба")
+    }
+}
