49430fbae5e534c4f2a30179c131ce94574ff705
korodiy <korodiy@yandex-team.ru>
2025-10-03 18:56:10 +0300

    KS-11-1 First classes: Первая задача на классы c исправлениями

--- src/main/kotlin/KS_Lesson11/KS-11-1 First classes.kt
+++ src/main/kotlin/KS_Lesson11/KS-11-1 First classes.kt
@@ -5,15 +5,15 @@ class User(
     val login: String,
     val password: String,
     val email: String,
-){
+) {
     fun printInfo() {
         println("Пользователь: id=$id, логин=$login, пароль=$password, email=$email")
     }
 }
 
 fun main() {
-    val myUser = User(0, "yndx.lora", "12345","mail@mail.ru")
-    val myOtherUser = User(1, "yndx.lera", "12345#","mail@gmail.ru")
+    val myUser = User(id = 0, login = "yndx.lora", password = "12345", email = "mail@mail.ru")
+    val myOtherUser = User(id = 1, login = "yndx.lera", password = "12345#", email = "mail@gmail.ru")
 
     myUser.printInfo()
     myOtherUser.printInfo()
