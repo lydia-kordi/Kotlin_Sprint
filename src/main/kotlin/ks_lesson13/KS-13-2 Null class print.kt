4f0a430165f9d85c22d020924a49542dba459604
korodiy <korodiy@yandex-team.ru>
2025-10-13 11:03:43 +0300

    KS-13-2 Null class print: Дополнила класс методом

--- src/main/kotlin/ks_lesson13/KS-13-2 Null class print.kt
+++ src/main/kotlin/ks_lesson13/KS-13-2 Null class print.kt
@@ -1,8 +1,14 @@
 package org.kotlin.sprint.ks_lesson13
 
-class TelephoneBook(val name: String, val phone: Long, val company: String? = null)
+class TelephoneBook(val name: String, val phone: Long, val company: String? = null) {
+
+    fun printInfo() {
+        println("${name}, ${phone}, ${company ?: "<Не указано>"}")
+    }
+}
 
 fun main() {
     val person = TelephoneBook(name = "Боря", phone = 79329384537, company = "Rambler")
-    println("${person.name}, ${person.phone}, ${person.company ?: "<Не указано>"}")
+    person.printInfo()
+
 }
