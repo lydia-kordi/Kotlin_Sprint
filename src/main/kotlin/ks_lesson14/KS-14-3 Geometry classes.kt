b830a1c67d73fbb87077d17086cd67dcbebd5e1e
korodiy <korodiy@yandex-team.ru>
2025-11-15 15:39:42 +0300

    KS-14-3 Geometry classes: Исправление замечаний

--- src/main/kotlin/ks_lesson14/KS-14-3 Geometry classes.kt
+++ src/main/kotlin/ks_lesson14/KS-14-3 Geometry classes.kt
@@ -1,62 +1,43 @@
 package org.kotlin.sprint.ks_lesson14
 
+const val Black = "Black"
+const val White = "White"
+
 abstract class Figure {
-    abstract val color: String?
+    abstract val color: String
     abstract fun calculateArea(): Double
     abstract fun calculatePerimeter(): Double
 }
 
 class Circle(
-    override val color: String?,
+    override val color: String,
     val radius: Int,
 ) : Figure() {
-    override fun calculateArea(): Double {
-        val circleArea = radius * radius * Math.PI
-        return circleArea
-    }
-
-    override fun calculatePerimeter(): Double {
-        val circlePerimeter = 2 * radius * Math.PI
-        return circlePerimeter
-    }
+    override fun calculateArea(): Double = radius * radius * Math.PI
+    override fun calculatePerimeter() = 2 * radius * Math.PI
 }
 
 class Rectangle(
-    override val color: String?,
+    override val color: String,
     val length: Int,
     val width: Int,
 ) : Figure() {
-    override fun calculateArea(): Double {
-        val rectangleArea = length * width.toDouble()
-        return rectangleArea
-    }
-
-    override fun calculatePerimeter(): Double {
-        val rectanglePerimeter = 2 * (length + width).toDouble()
-        return rectanglePerimeter
-    }
+    override fun calculateArea(): Double = length * width.toDouble()
+    override fun calculatePerimeter(): Double = 2 * (length + width).toDouble()
 }
 
 fun main() {
-    val circle1 = Circle(color = "Black", radius = 2)
-    val circle2 = Circle(color = "White", radius = 3)
+    val circle1 = Circle(color = Black, radius = 2)
+    val circle2 = Circle(color = White, radius = 3)
 
-    val rectangle1 = Rectangle(color = "Black", length = 10, width = 15)
-    val rectangle2 = Rectangle(color = "White", length = 12, width = 19)
-    val rectangle3 = Rectangle(color = "Black", length = 5, width = 21)
+    val rectangle1 = Rectangle(color = Black, length = 10, width = 15)
+    val rectangle2 = Rectangle(color = White, length = 12, width = 19)
+    val rectangle3 = Rectangle(color = Black, length = 5, width = 21)
 
-    val figuresList: List<Figure?> = listOf(circle1, circle2, rectangle1, rectangle2, rectangle3)
+    val figuresList: List<Figure> = listOf(circle1, circle2, rectangle1, rectangle2, rectangle3)
 
-    var blackPerimeter = 0.0
-    var whiteArea = 0.0
-
-    for (figure in figuresList) {
-        when (figure?.color) {
-            "White" -> whiteArea += figure.calculateArea()
-            "Black" -> blackPerimeter += figure.calculatePerimeter()
-            else -> {}
-        }
-    }
+    val blackPerimeter = figuresList.filter { it.color == Black }.sumOf { it.calculatePerimeter() }
+    val whiteArea= figuresList.filter { it.color == White }.sumOf { it.calculateArea() }
 
     println("Сумма периметров всех чёрных фигур: $blackPerimeter")
     println("Сумма площадей всех белых фигур: $whiteArea")
